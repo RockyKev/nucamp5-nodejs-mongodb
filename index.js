1477b8380d299e4116b5fdcc9afdb884e266e7d9
RockyKev <kev.rocky@gmail.com>
2019-12-03 21:27:29 -0800

    Mongo Example 2 - callback hell

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
+const dbOperate = require('./operations')
 
 const url = 'mongodb://localhost:27017/';
 const dbname = 'conFusion';
@@ -13,28 +14,33 @@ MongoClient.connect(url, (err, client) => {
     console.log('Connected correctly to server');
 
     const db = client.db(dbname);
-    const collection = db.collection('dishes');
-    collection.insertOne({'name': 'Uthapizza', 'description': 'test'}, 
-    (err, result) => {
-        assert.equal(err, null);
 
-        console.log("After Insert:\n");
-        console.log(result.ops); //shows how many operations have been carried out correctly
+    dbOperate.insertDocument(db, { name: "Vadonut", description: "Test"}, 'dishes', (result) => {
+        
+        console.log('inserted Document:\n ', result.ops);
 
-        collection.find({}).toArray((err, docs) => {
-            assert.equal(err, null);
+        dbOperate.findDocuments(db, 'dishes', (docs) => {
+            console.log('Found Documents:\n', docs);
+        
+            dbOperate.updateDocument(db, {name: "Vadonut"}, {description : "Updated Test"}, 'dishes', (result) => {
+                console.log('Updated Documents:\n', result.result);
 
-            console.log('Found: \n');
-            console.log(docs);
+                dbOperate.findDocuments(db, 'dishes', (docs) => {
+                    console.log('Found Documents:\n', docs);
 
-            db.dropCollection('dishes', (err, result) => {
-                assert.equal(err, null);
+                    db.dropCollection('dishes', (result) => {
+                        console.log("Dropped collection: ", result);
 
-                client.close();                
-            });
-        });
-    });
+                        client.close();
+                    });
+                })
+            
+            })
+        
+        })
 
+
+    })
 
 });
 
